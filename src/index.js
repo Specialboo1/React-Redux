2c51ba87d6e07051e97a933d94806c68c4f9c91d
Specialboo1 <boopalan1314@gmail.com>
2022-09-07 13:09:00 +0530

    React-Redux first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>  <App /></Provider>  
   </React.StrictMode>
 );
 
